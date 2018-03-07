6f6997dd8805862641da917714b60a694955a7e8
kristjankostelec <kristjan.kostelec@gmail.com>
2018-03-07 10:00:02 +0100

    vaje2
    nadaljuj doma :)

---
+++ Vaja2/src/Golovec.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Golovec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static Integer kazni(String vhodna, String izhodna) throws IOException{
+		
+		BufferedReader vhod = new BufferedReader(new FileReader(vhodna));
+		PrintWriter izhod = new PrintWriter(new FileWriter(izhodna));
+		
+		while (vhod.ready()){
+			
+			
+			
+		}
+		
+	}
+	
+
+}
